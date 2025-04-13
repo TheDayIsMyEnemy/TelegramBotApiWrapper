2c6479915e3b78f9bae276ded840c679a7eab5c8
TheDayIsMyEnemy <tthedayismyenemy@gmail.com>
2025-04-13 21:26:00 +0300

    Upgrade to .net9.0

--- TelegramBotApiWrapper/TelegramBotApiClient.cs
+++ TelegramBotApiWrapper/TelegramBotApiClient.cs
@@ -5,53 +5,52 @@ using System.Net.Http.Json;
 using TelegramBotApiWrapper.Requests;
 using TelegramBotApiWrapper.Responses;
 
-namespace TelegramBotApiWrapper
+namespace TelegramBotApiWrapper;
+
+public class TelegramBotApiClient : ITelegramBotApiClient
 {
-    public class TelegramBotApiClient : ITelegramBotApiClient
+    private readonly HttpClient _httpClient;
+    private readonly ITelegramBotApiClientOptions _options;
+
+    public TelegramBotApiClient(
+        HttpClient httpClient,
+        ITelegramBotApiClientOptions options)
     {
-        private readonly HttpClient _httpClient;
-        private readonly ITelegramBotApiClientOptions _options;
+        _httpClient = httpClient;
+        _options = options;
+    }
 
-        public TelegramBotApiClient(
-            HttpClient httpClient,
-            ITelegramBotApiClientOptions options)
-        {
-            _httpClient = httpClient;
-            _options = options;
-        }
+    public async Task<GetMeResponse?> GetMe()
+    {
+        using HttpResponseMessage res = await _httpClient.GetAsync(
+                $"{_options.BaseUrl}/bot{_options.ApiKey}/getMe");
 
-        public async Task<GetMeResponse?> GetMe()
-        {
-            using HttpResponseMessage res = await _httpClient.GetAsync(
-                    $"{_options.BaseUrl}/bot{_options.ApiKey}/getMe");
+        res.EnsureSuccessStatusCode();
 
-            res.EnsureSuccessStatusCode();
+        return await res.Content.ReadFromJsonAsync<GetMeResponse?>();
+    }
 
-            return await res.Content.ReadFromJsonAsync<GetMeResponse?>();
-        }
+    public async Task<GetUpdatesResponse?> GetUpdates()
+    {
+        using HttpResponseMessage res = await _httpClient.GetAsync(
+                $"{_options.BaseUrl}/bot{_options.ApiKey}/getUpdates");
 
-        public async Task<GetUpdatesResponse?> GetUpdates()
-        {
-            using HttpResponseMessage res = await _httpClient.GetAsync(
-                    $"{_options.BaseUrl}/bot{_options.ApiKey}/getUpdates");
+        res.EnsureSuccessStatusCode();
 
-            res.EnsureSuccessStatusCode();
+        return await res.Content.ReadFromJsonAsync<GetUpdatesResponse?>();
+    }
 
-            return await res.Content.ReadFromJsonAsync<GetUpdatesResponse?>();
-        }
+    public async Task<HttpStatusCode> SendMessage(SendMessageRequest req)
+    {
+        using StringContent jsonContent = new(
+                JsonSerializer.Serialize(req),
+                Encoding.UTF8,
+                "application/json");
 
-        public async Task<HttpStatusCode> SendMessage(SendMessageRequest req)
-        {
-            using StringContent jsonContent = new(
-                    JsonSerializer.Serialize(req),
-                    Encoding.UTF8,
-                    "application/json");
+        using HttpResponseMessage res = await _httpClient.PostAsync(
+                $"{_options.BaseUrl}/bot{_options.ApiKey}/sendMessage",
+                jsonContent);
 
-            using HttpResponseMessage res = await _httpClient.PostAsync(
-                    $"{_options.BaseUrl}/bot{_options.ApiKey}/sendMessage",
-                    jsonContent);
-
-            return res.StatusCode;
-        }
+        return res.StatusCode;
     }
 }
--- samples/MinimalWebApi/Program.cs
+++ samples/MinimalWebApi/Program.cs
@@ -2,14 +2,28 @@ using TelegramBotApiWrapper;
 using TelegramBotApiWrapper.Requests;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<ITelegramBotApiClientOptions, TelegramBotApiClientOptions>((serviceProvider) => new TelegramBotApiClientOptions { ApiKey = builder.Configuration["TelegramBotApiClientOptions:ApiKey"]! });
+builder.Services.AddSingleton<ITelegramBotApiClientOptions, TelegramBotApiClientOptions>(
+    serviceProvider => new TelegramBotApiClientOptions
+    {
+        ApiKey = builder.Configuration["TelegramBotApiClientOptions:ApiKey"]!
+    });
 builder.Services.AddHttpClient<ITelegramBotApiClient, TelegramBotApiClient>();
+
 var app = builder.Build();
-if (app.Environment.IsDevelopment()){ app.UseSwagger(); app.UseSwaggerUI(); }
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseHttpsRedirection();
+
 app.MapGet("/getMe", async (ITelegramBotApiClient client) => { var res = await client.GetMe(); return res; }).WithName("GetMe").WithOpenApi();
 app.MapGet("/getUpdates", async (ITelegramBotApiClient client) => { var res = await client.GetUpdates(); return res; }).WithName("GetUpdates").WithOpenApi();
 app.MapPost("/sendMessage", async (ITelegramBotApiClient client, SendMessageRequest req) => { var res = await client.SendMessage(req); return TypedResults.StatusCode((int)res); }).WithName("SendMessage").WithOpenApi();
+
 app.Run();
